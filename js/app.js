65c75e05feb14c29e097243b816aae5b437fc5d5
lilly <lillianvioletmcbride@gmail.com>
2020-07-02 18:57:26 -0500

    create products

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ var product3 = 2;
 var allProducts = [];
 var totalClicks = 0;
 var rounds = 25;
-// Constructor
+
+// constructor
 function Product(name, imageUrl) {
   this.name = name;
   this.imageUrl = imageUrl;
@@ -15,3 +16,25 @@ function Product(name, imageUrl) {
   this.timesSeen = 0;
   allProducts.push(this);
 }
+
+// actually create products
+new Product('Bag', 'images/bag.jpg');
+new Product('Banana Slicer', 'images/banana.jpg');
+new Product('Tablet Stand', 'images/bathroom.jpg');
+new Product('Toeless Boots', 'images/boots.jpg');
+new Product('Breakfast Maker', 'images/breakfast.jpg');
+new Product('Meatball Bubble Gum', 'images/bubblegum.jpg');
+new Product('Chair', 'images/chair.jpg');
+new Product('Cthulhu', 'images/cthulhu.jpg');
+new Product('Duck Muzzle', 'images/dog-duck.jpg');
+new Product('Dragon Meat', 'images/dragon.jpg');
+new Product('Pen Silverware', 'images/pen.jpg');
+new Product('Pet Sweeper', 'images/pet-sweep.jpg');
+new Product('Pizza Scissors', 'images/scissors.jpg');
+new Product('Shark Sleeping Bag', 'images/shark.jpg');
+new Product('Baby Sweeper', 'images/sweep.png');
+new Product('Tauntain Sleeping Bag', 'images/tauntaun.jpg');
+new Product('Unicorn Meat', 'images/unicorn.jpg');
+new Product('Tentacle Flashdrive', 'images/usb.gif');
+new Product('Watering Can', 'images/water-can.jpg');
+new Product('Wine Glass', 'images/wine-glass.jpg');
